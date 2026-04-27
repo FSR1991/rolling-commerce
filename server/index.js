2c3dfd080d1c6cb6547e325977bf3473f4430438
facundosolano11 <facundosolanorodriguez@gmail.com>
2026-04-27 18:49:24 -0300

    modulo order listo para integracion

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import connectDB from "./src/config/db.js";
 // import authRoutes from "./src/routes/authRoutes.js";
 import userRoutes from "./src/routes/userRoutes.js";
 import cartRoutes from "./src/routes/cartRoutes.js";
+import orderRoutes from "./src/routes/orderRoutes.js";
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 // app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/cart",cartRoutes);
+app.use("/api/orders", orderRoutes);
 
 
 //ruta base
